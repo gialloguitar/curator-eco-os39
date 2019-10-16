8e8584448ae6c21344bd5bcbe267148ba7daf47a
Vladimir Pereskokov <vladimir_pereskokov@epam.com>
2019-10-16 13:07:59 +0300

    Edit util

--- app/util.py
+++ app/util.py
@@ -6,8 +6,23 @@ import logging
 from pprint import pprint
 
 def create_logger(name):
+
+    levelNames = {0: 'NOTSET',
+                 10: 'DEBUG',
+                 20: 'INFO',
+                 30: 'WARNING',
+                 40: 'ERROR',
+                 50: 'CRITICAL',
+                 'CRITICAL': 50,
+                 'DEBUG': 10,
+                 'ERROR': 40,
+                 'INFO': 20,
+                 'NOTSET': 0,
+                 'WARN': 30,
+                 'WARNING': 30}
+
     logger = logging.getLogger(name)
-    lvl = logging._levelNames.get(os.getenv('CURATOR_SCRIPT_LOG_LEVEL', 'INFO'), None)
+    lvl = levelNames.get(os.getenv('CURATOR_SCRIPT_LOG_LEVEL', 'INFO'), None)
     if not lvl:
         print >> sys.stderr, 'The CURATOR_SCRIPT_LOG_LEVEL must be one of CRITICAL, ERROR, WARNING, INFO or DEBUG'
         return None
@@ -18,4 +33,7 @@ def create_logger(name):
     logger.addHandler(lh)
     return logger
 
-pprint(logging._levelNames.get(os.getenv('CURATOR_SCRIPT_LOG_LEVEL'), "NotFound!"))
+l = create_logger(__name__)
+print(l.getEffectiveLevel())
+
+
